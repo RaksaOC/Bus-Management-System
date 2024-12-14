2e9b647e167e136b83d9621d71ac21079c666ef6
rong <darong6126@gmail.com>
2024-12-14 21:10:01 +0700

    done with delete user function

--- main/main.cpp
+++ main/main.cpp
@@ -12,8 +12,12 @@ using namespace std;
 
 int main()
 {
-    System sys;
-    User *user = sys.authenticateUser();
-    user->checkUserType();
+    User user;
+    user.deleteUser();
+    // System sys;
+    // User *user = sys.authenticateUser();
+    // user->checkUserType();
+
+    
     return 0;
 }
